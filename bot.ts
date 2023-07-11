9b19a2d60bae9419d8f91237dd0362f48ec095bf
levch <levchenko.d.a1998@gmail.com>
2023-07-11 08:36:25 +0600

    -

---
+++ bot.ts
@@ -0,0 +1,55 @@
+import {
+  Bot as _Bot,
+  Context,
+  DenoKVAdapter,
+  env,
+  hydrateReply,
+  parseMode,
+  run,
+  sequentialize,
+  session,
+} from "./deps.ts"
+import { BaseContext, BaseSession } from "./types.ts"
+
+export default class Bot<S extends BaseSession> extends _Bot<BaseContext<S>> {
+  constructor(token: string, defaultSession: S) {
+    super(token)
+    this.use(sequentialize(getSessionKey))
+    this.initParseModePlugin()
+    this.initSessionPlugin(defaultSession)
+  }
+
+  initParseModePlugin() {
+    this.api.config.use(parseMode("HTML"))
+    this.use(hydrateReply)
+  }
+
+  initSessionPlugin(defaultSession: S) {
+    this.use(createSessionMiddleware(defaultSession))
+  }
+
+  static fromEnv<S extends BaseSession>(defaultSession: S) {
+    return new Bot(env.str("TOKEN"), defaultSession)
+  }
+
+  run() {
+    run(this)
+  }
+}
+
+// @ts-ignore: TODO
+const kv = await Deno.openKv()
+const storage = new DenoKVAdapter(kv)
+
+function getSessionKey(ctx: Context): string | undefined {
+  if (ctx.from == undefined || ctx.chat == undefined) return
+  return `${ctx.chat.id}/${ctx.from.id}`
+}
+
+function createSessionMiddleware(defaultSession: BaseSession) {
+  return session({
+    initial: () => structuredClone(defaultSession),
+    getSessionKey,
+    storage,
+  })
+}
--- deps.ts
+++ deps.ts
@@ -1,9 +1,21 @@
 export {
+  Bot,
   Composer,
   Context,
   InlineKeyboard,
+  session,
   type SessionFlavor,
 } from "https://deno.land/x/grammy@v1.17.2/mod.ts"
+export {
+  hydrateReply,
+  parseMode,
+} from "https://deno.land/x/grammy_parse_mode@1.7.1/mod.ts"
 export type { ParseModeFlavor } from "https://deno.land/x/grammy_parse_mode@1.7.1/mod.ts"
+export {
+  run,
+  sequentialize,
+} from "https://deno.land/x/grammy_runner@v2.0.3/mod.ts"
+export { DenoKVAdapter } from "https://deno.land/x/grammy_storages@v2.3.0/denokv/src/mod.ts"
+export { default as env } from "https://deno.land/x/parse_env@v0.0.3/mod.ts"
 export { sleep } from "https://deno.land/x/sleep@v1.2.1/mod.ts"
 export { Observer } from "./observer.ts"
